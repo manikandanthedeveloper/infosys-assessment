b2afd602c047b3285e3e57243f9cd12f6a29423f
Manikandan Dhanapal <manikandanthedeveloper@users.noreply.github.com>
2026-06-28 19:48:30 +0530

    reward calculator unit test implemented

---
+++ src/tests/rewardCalculator.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "vitest";
+import rewardCalculator from "../utils/rewardCalculator";
+
+describe("rewardCalculator", () => {
+	test("returns 90 points for $120", () => {
+		expect(rewardCalculator(120)).toBe(90);
+	});
+});
